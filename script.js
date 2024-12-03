76c8bd752cc4e19eb6a95bb0249656b0176b6107
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-03 21:58:58 +0330

    1672. Richest Customer Wealth

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+// 1672. Richest Customer Wealth
+// https://leetcode.com/problems/richest-customer-wealth/description/
+function maximumWealth(accounts) {
+    return Math.max.apply(Math, accounts.map(function (customer) { return customer.reduce(function (sum, cur) { return sum + cur; }); }, 0));
+}
--- script.ts
+++ script.ts
@@ -1,1 +1,10 @@
 "use strict";
+
+// 1672. Richest Customer Wealth
+// https://leetcode.com/problems/richest-customer-wealth/description/
+
+function maximumWealth(accounts: number[][]): number {
+  return Math.max(
+    ...accounts.map((customer) => customer.reduce((sum, cur) => sum + cur), 0)
+  );
+}
