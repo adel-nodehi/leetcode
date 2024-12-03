58a779bdde83296b331cb4d7b5d9f54327a797a7
Adel Nodehi <adelnmo2003@gmail.com>
2024-12-03 22:27:34 +0330

    2037. Minimum Number of Moves to Seat Everyone

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 "use strict";
+/*
 // 1672. Richest Customer Wealth
 // https://leetcode.com/problems/richest-customer-wealth/description/
-function maximumWealth(accounts) {
-    return Math.max.apply(Math, accounts.map(function (customer) { return customer.reduce(function (sum, cur) { return sum + cur; }); }, 0));
+
+function maximumWealth(accounts: number[][]): number {
+  return Math.max(
+    ...accounts.map((customer) => customer.reduce((sum, cur) => sum + cur), 0)
+  );
+}
+*/
+// 2037. Minimum Number of Moves to Seat Everyone
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+function minMovesToSeat(seats, students) {
+    seats.sort(function (a, b) { return a - b; });
+    students.sort(function (a, b) { return a - b; });
+    return seats.reduce(function (moves, seat, i) { return moves + Math.abs(seat - students[i]); }, 0);
 }
--- script.ts
+++ script.ts
@@ -1,10 +1,25 @@
 "use strict";
 
+/*
 // 1672. Richest Customer Wealth
 // https://leetcode.com/problems/richest-customer-wealth/description/
 
 function maximumWealth(accounts: number[][]): number {
   return Math.max(
     ...accounts.map((customer) => customer.reduce((sum, cur) => sum + cur), 0)
+  );
+}
+*/
+
+// 2037. Minimum Number of Moves to Seat Everyone
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/description/
+
+function minMovesToSeat(seats: number[], students: number[]): number {
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
+
+  return seats.reduce(
+    (moves, seat, i) => moves + Math.abs(seat - students[i]),
+    0
   );
 }
